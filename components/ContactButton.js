5b4bf1dcc8a0a68d3d9680768da6f477b97eb825
Declan Rapp <declanrapp@gmail.com>
2023-07-12 09:08:47 -0400

    Minor fixes

--- components/ContactButton.js
+++ components/ContactButton.js
@@ -3,7 +3,7 @@ import { Button, Card } from 'flowbite-react';
 
 export default function ContactButton(props) {
     return (
-        <Button href={'/about'} size='lg' pill color="whitesmoke" className="transition-all duration-1000 mt-8 bg-goldenrod hover:bg-whitesmoke text-whitesmoke hover:text-black shadow-shine-lg hover:shadow-[0_0_10px_5px_goldenrod] border border-white hover:border-black">
+        <Button href={'/about'} size='md' pill color="whitesmoke" className="transition-all duration-1000 mt-8 bg-goldenrod hover:bg-whitesmoke text-whitesmoke hover:text-black shadow-shine-lg hover:shadow-[0_0_10px_5px_goldenrod] border border-white hover:border-black">
             Get in Touch
         </Button>
     )
