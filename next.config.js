d4fd433963528ebf569120f2d9434c3ab4d6900a
Declan Rapp <declanrapp@gmail.com>
2023-07-21 14:42:39 -0400

    Using local env

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+    },
+    env: {
+        ENVIRONMENT: "DEV",
+        API_ENDPOINT: 'http://localhost:3001/'
+    }
+}
+
+module.exports = nextConfig
