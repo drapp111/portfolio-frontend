d3467dd8a2d7ed55fdff66462876dd9a987ff833
drapp111 <60412784+drapp111@users.noreply.github.com>
2024-01-18 14:02:59 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@
 const nextConfig = {
     experimental: {
         serverActions: true,
-    },
-    env: {
-        ENVIRONMENT : "DEV",
-        API_ENDPOINT: 'http://localhost:3001/',
-      }
+    }
 }
 
 module.exports = nextConfig
